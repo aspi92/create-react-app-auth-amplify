c95dbce9f065a7fd8b798ddb3ac0652138ecc20b
EC2 Default User <ec2-user@ip-172-31-5-111.eu-central-1.compute.internal>
2020-10-07 14:26:41 +0000

    Added basic support for writing to Analytics Data Stream

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify from 'aws-amplify';
+import Amplify, {Analytics, AWSKinesisProvider} from 'aws-amplify';
 import aws_exports from './aws-exports';
 import { BookingList } from './BookingList.jsx';
 
 Amplify.configure(aws_exports);
+
+
+Analytics.configure({
+    AWSKinesis: {
+
+        // OPTIONAL -  Amazon Kinesis service region
+        region: 'eu-central-1',
+
+        // OPTIONAL - The buffer size for events in number of items.
+        bufferSize: 1000,
+
+        // OPTIONAL - The number of events to be deleted from the buffer when flushed.
+        flushSize: 100,
+
+        // OPTIONAL - The interval in milliseconds to perform a buffer check and flush if necessary.
+        flushInterval: 5000, // 5s
+
+        // OPTIONAL - The limit for failed recording retries.
+        resendLimit: 5
+    } 
+});
+Analytics.addPluggable(new AWSKinesisProvider());
 
 class App extends Component {
   render() {
--- src/BookingList.jsx
+++ src/BookingList.jsx
@@ -1,21 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import API from '@aws-amplify/api';
+import { Analytics } from 'aws-amplify';
 
 export function BookingList() {
     const [bookings, setBookings] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        Analytics.record({
+            data: {
+                "event": "Loading bookings",
+            },
+            streamName: 'octankairlinestream-devx'
+        }, 'AWSKinesis');
         API.endpoint('octankapi').then((endpoint) => console.log(endpoint))
         API.get('octankapi', '/bookings')
             .then((bookingsResponse) => {
                 setBookings(bookingsResponse);
                 setIsLoading(false);
+                Analytics.record({
+                    data: {
+                        "event": "Successfully loaded bookings",
+                    },
+                    streamName: 'octankairlinestream-devx'
+                }, 'AWSKinesis');
             })
             .catch((error) => {
                 console.log('Error fetching bookings', error);
                 setBookings(null);
                 setIsLoading(false);
+                Analytics.record({
+                    data: {
+                        "event": "Failed to load bookings",
+                    },
+                    streamName: 'octankairlinestream-devx'
+                }, 'AWSKinesis');
             })
     }, [])
 
